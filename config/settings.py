62b63b58692e372ecbfae7ca14d2076efe5d7409
Oteri Eyenike <teyenike1@gmail.com>
2024-06-26 15:07:10 +0100

    Fix: add asterisk to the allowed host

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-anyctl^cv=u5oma9s59+@gd8z+wm+^zfl!0!!!1q$g71jl!cro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://xata-link-shortner.onrender.com/']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
