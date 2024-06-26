478ccc81ef93bf868c3d3b2b96c61346541d2f7b
Oteri Eyenike <teyenike1@gmail.com>
2024-06-26 16:58:01 +0100

    Fix: refactor code

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,11 @@ SECRET_KEY = 'django-insecure-anyctl^cv=u5oma9s59+@gd8z+wm+^zfl!0!!!1q$g71jl!cro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'xata-link-shortner.onrender.com',
+    '127.0.0.1',
+    'localhost',
+]
 
 # Application definition
 
